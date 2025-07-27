90b8dfc0362f4be543142da47fb7574e0a12e00a
Lucas Becker <runxel@users.noreply.github.com>
2025-07-27 13:50:45 +0200

    add some examples

---
+++ examples/examples.py
@@ -0,0 +1,33 @@
+from time import sleep
+from perisso import perisso, clearhighlight, Filter, ElType
+
+# initialize an ElementCollection
+elements = perisso()
+
+# string representation
+print(elements)
+
+# get the number of elements in the set
+print(len(elements))
+
+# check with its GUID if an element is the ElementCollection
+print("216E9CE2-8007-334A-9D8C-FB0EC7EC083C" in elements)
+
+# get only beams
+beams_only = elements.filterBy(Filter.ELEMENT_TYPE).equals("Beam").get()
+print(beams_only)
+
+# you can also chain multiple filters
+col_elements = (
+	elements.filterBy(Filter.ELEMENT_TYPE)
+	.equals(ElType.COLUMN)
+	.filterBy(Filter.ID)
+	.contains("123")
+	.get()
+)
+print(col_elements)
+
+# additional functionalities:
+perisso().filterBy(Filter.PROPERTY).property("Prop Group Name", "Prop Name").equals("test").highlight()
+sleep(5)
+clearhighlight()
