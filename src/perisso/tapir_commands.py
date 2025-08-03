2da0dbedf3038755c4775d85e94473546efd108d
Lucas Becker <runxel@users.noreply.github.com>
2025-08-03 17:45:08 +0200

    Add commands

--- src/perisso/tapir_commands.py
+++ src/perisso/tapir_commands.py
@@ -90,6 +90,252 @@ class TapirCommands:
 				self.act.AddOnCommandId("TapirCommand", command)
 			)
 
+	# region Application Commands
+	def getTapirVersion(self) -> str:
+		"""Retrieves the version of the installed Tapir Add-On."""
+		name_ = "GetAddOnVersion"
+		return self._run(name_)["version"]
+
+	def getArchicadLocation(self) -> str:
+		"""Retrieves the location of the currently running Archicad executable."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)["archicadLocation"]
+
+	def quitArchicad(self) -> dict:
+		"""Performs a quit operation on the currently running Archicad instance."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def getCurrentWindowType(self) -> str:
+		"""Returns the type of the current (active) window."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)["currentWindowType"]
+
+	# endregion
+	# region 	Project Commands
+	def getProjectInfo(self) -> dict:
+		"""Retrieves information about the currently loaded project."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def getProjectInfoFields(self) -> dict:
+		"""Retrieves the names and values of all project info fields."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def getStories(self) -> dict:
+		"""Retrieves information about the story sructure of the currently loaded project."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def getHotlinks(self) -> dict:
+		"""Gets the file system locations (path) of the hotlink modules. The hotlinks can have tree hierarchy in the project."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def openProject(self, path: str | Path) -> dict:
+		"""Opens the given project."""
+		name_ = inspect.currentframe().f_code.co_name
+		path = _validate_path(path, io_op="r")
+		params = {"projectFilePath": path}
+		return self._run(name_, params)
+
+	def getGeoLocation(self) -> dict:
+		"""Gets the project location details."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	# endregion
+	# region 	Element Commands
+
+	# The following commands are not integrated, since Perisso covers it:
+	# 	GetSelectedElements
+	# 	GetElementsByType
+	# 	GetAllElements
+	# 	ChangeSelectionOfElements
+	# 	FilterElements
+
+	def getDetailsOfElements(self, elements: List[Dict[str, str]]) -> dict:
+		"""Get details of given elements."""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {"elements": elements}
+		return self._run(name_, params)
+	def get3DBoundingBoxes(self, elements: List[Dict[str, str]]) -> Dict[str, Any]:
+		"""Get 3D bounding boxes of elements."""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {"elements": elements}
+		return self._run(name_, params)
+
+	def getConnectedElements(
+		self, elements: List[Dict[str, str]], connectedElementType: ElType
+	) -> dict:
+		"""Gets the subelements of the given hierarchical elements."""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {
+			"elements": elements,
+			"connectedElementType": connectedElementType.value,
+		}
+		return self._run(name_, params)
+
+	def highlightElements(
+		self,
+		elements: Dict[str, str] = None,
+		*,
+		hightlightcolor: list[list[int]] = [[77, 235, 103, 100]],
+		mutedcolor: list[int] = [164, 166, 165, 128],
+		wireframe=True,
+	) -> None:
+		"""Highlight the elements in the given color and mutes all other elements."""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {
+			"elements": elements,
+			"highlightedColors": hightlightcolor,
+			"wireframe3D": wireframe,
+			"nonHighlightedColor": mutedcolor,
+		}
+		return self._run(name_, params)
+
+	def clearHighlight(self):
+		"""Clear all highlighting in Archicad."""
+		# A helper function – this does not exist as it's own Tapir command.
+		return self.highlightElements(elements=[None])
+
+	def moveElements(
+		self, elements: Dict[str, str], vector: Vector, copy: bool = False
+	):
+		"""Moves elements with a given vector."""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {
+			"elementsWithMoveVectors": [
+				{
+					"elementId": el["elementId"],
+					"moveVector": vector.to_3d().to_dict(),
+					"copy": copy,
+				}
+				for el in elements
+			]
+		}
+		return self._run(name_, params)
+
+	def getGDLParametersOfElements(self, elements: List[Dict[str, str]]) -> dict:
+		"""Gets all the GDL parameters (name, type, value) of the given elements."""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {"elements": elements}
+		return self._run(name_, params)
+	# endregion
+	# region 	Create Elements
+	def createColumns(self, coors: List[Coordinate]) -> dict:
+		"""Creates Column elements based on the given parameters."""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {
+			"columnsData": [{"coordinates": coor.to_3d().to_dict()} for coor in coors]
+		}
+		return self._run(name_, params)
+	# endregion
+	# region 	Favorites Commands
+	def applyFavoritesToElementDefaults(self, favoriteNames: str) -> dict:
+		"""Apply the given favorites to their respective element tool defaults."""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {"favorites": [favoriteNames]}
+		return self._run(name_, params)
+
+	def createFavoritesFromElements(
+		self, elements: List[Dict[str, str]], favoriteNames: List[str]
+	) -> dict:
+		"""Create favorites from the given elements."""
+		name_ = inspect.currentframe().f_code.co_name
+		if isinstance(favoriteNames, str):
+			# make sure we get a list
+			favoriteNames = [favoriteNames]
+		params = {
+			"favoritesFromElements": [
+				{
+					"elementId": el["elementId"],
+					"favorite": favname,
+				}
+				for el, favname in zip(elements, favoriteNames)
+			]
+		}
+		return self._run(name_, params)
+
+	# endregion
+	# region 	Property Commands
+	def getAllProperties(self) -> dict:
+		"""Returns all user defined and built-in properties."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def getPropertyValuesOfElements(
+		self, elements: List[Dict[str, str]], propertyGUIDs: List[str]
+	) -> Dict[str, Any]:
+		"""
+		Get property values of elements.
+
+		Args:
+			elements: List of element dictionaries with identification info
+			properties: List of property parameters with propertyId mappings
+
+		Returns:
+			Dictionary containing the property values for the requested elements
+		"""
+		name_ = inspect.currentframe().f_code.co_name
+		params = {
+			"elements": elements,
+			"properties": [{"propertyId": {"guid": pg}} for pg in propertyGUIDs],
+		}
+		return self._run(name_, params)
+	# endregion
+	# region 	Attribute Commands
+	# endregion
+	# region 	Library Commands
+	def getLibraries(self) -> Dict[str, Any]:
+		"""Gets the list of loaded libraries."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def reloadLibraries(self) -> Dict[str, Any]:
+		"""Executes the reload libraries command."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	# endregion
+	# region 	Teamwork Commands
+	def _twTest(self, command: str) -> bool:
+		"""Internal function that will warn if a file is not a TW file."""
+		if not self.getProjectInfo()["isTeamwork"]:
+			_printcol(f"Not a Teamwork file! Command »{command}« will not be sent.")
+			return False
+		return True
+
+	def teamworkSend(self) -> Dict[str, Any]:
+		"""Performs a send operation on the currently opened Teamwork project."""
+		name_ = inspect.currentframe().f_code.co_name
+		if self._twTest(name_):
+			return self._run(name_)
+		return None
+	# endregion
+	# region 	Navigator Commands
+
+	# endregion
+	# region 	Issue Management Commands
+	# endregion
+	# region 	Revision Management Commands
+	def getRevisionIssues(self) -> Dict[str, Any]:
+		"""Retrieves all issues."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def getRevisionChanges(self) -> Dict[str, Any]:
+		"""Retrieves all changes."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+
+	def getDocumentRevisions(self) -> Dict[str, Any]:
+		"""Retrieves all document revisions."""
+		name_ = inspect.currentframe().f_code.co_name
+		return self._run(name_)
+	# endregion
+
 
 # Export a singleton
 tapir = TapirCommands()
