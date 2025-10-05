259f6d120ba9ebd2d056557726c622313cd9beac
Lucas Becker <runxel@users.noreply.github.com>
2025-10-05 23:17:41 +0200

    Update __init__.py to include PropertyDataType and Polygon in exports

--- src/perisso/__init__.py
+++ src/perisso/__init__.py
@@ -2,20 +2,22 @@
 
 from .core import perisso
 from .tapir_commands import tapir
-from .enums import Filter, ElType, AttrType, ProjectInfo
-from .types import Vector, Coordinate, Arc, Polyline, Color
+from .enums import Filter, ElType, AttrType, PropertyDataType, ProjectInfo
+from .types import Vector, Coordinate, Arc, Polyline, Polygon, Color
 
 
 __all__ = [
 	"perisso",
+	"tapir",
 	"Filter",
 	"ElType",
 	"AttrType",
+	"PropertyDataType",
 	"ProjectInfo",
-	"tapir",
 	"Vector",
 	"Coordinate",
 	"Arc",
 	"Polyline",
+	"Polygon",
 	"Color",
 ]
