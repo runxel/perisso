68c95c80f0963bb0bc753d7680e02c9d5c0e15a5
Lucas Becker <runxel@users.noreply.github.com>
2026-02-15 19:53:58 +0100

    Improve error message for Archicad connection failure

--- src/perisso/connection.py
+++ src/perisso/connection.py
@@ -4,7 +4,7 @@ from archicad.releases import Commands, Types, Utilities
 # Connection setup
 conn = ACConnection.connect()
 if not conn:
-	raise Exception("No Archicad instance running!")
+	raise Exception("No Archicad instance running, or open modal dialog!")
 
 acc: Commands = conn.commands
 acu: Utilities = conn.utilities
